1305fd9f2cf6c939d8df429add9e2a373815de19
jdhofmann <justindhofmann@gmail.com>
2021-01-14 21:11:55 -0500

    backup without scroll

---
+++ main.js
@@ -0,0 +1,23 @@
+// window.onscroll = function() {
+//     myFunction()
+
+// };
+
+
+let containers = document.querySelectorAll(".container")
+
+console.log(containers)
+
+// let sticky = c0.offsetTop;
+
+// function myFunction() {
+//     console.log(window.pageYOffset)
+//     containers.forEach(
+//         item => {
+//             if(window.pageYOffset + 200 > item.offsetTop) {
+//                 item.classList.add("focused")
+//             } 
+//         } 
+//     )
+// }
+
